179785148d1bff23e0f78f9380948bafe0cd1378
donggyulee <nninnnin7@gmail.com>
2025-07-14 17:12:33 +0900

    Add index page

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { app, BrowserWindow } = require("electron");
+
 console.log("Hello, Syncraft!");
+
+const createWindow = () => {
+  const browserWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  browserWindow.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
