462f4f2e4d7017a50764dc5f1b2fb946abf126b7
이동규 (Justin) <nninnnin7@gmail.com>
2025-10-16 16:10:20 +0900

    Add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 export default function Home() {
   return (
     <div>
-      <h1>Syncraft</h1>
+      <h1 className="bg-green-100 p-3 font-bold">Syncraft</h1>
 
-      <div>소스에서 가져온 리스트</div>
-      <div>변환된 리스트</div>
+      <div className="flex">
+        <div className="flex-1 p-[1em]">
+          <h3>소스에서 가져온 리스트</h3>
+        </div>
+
+        <div className="flex-1 p-[1em]">
+          <h3>변환된 리스트</h3>
+        </div>
+      </div>
     </div>
   );
 }
